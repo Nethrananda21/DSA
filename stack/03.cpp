006c3a8506cc6ff04566d1ab713b783f37e23d4e
Nethrananda21 <reddynethrananda@gmail.com>
2026-02-14 23:40:50 +0530

    .

--- stack/03.cpp
+++ stack/03.cpp
@@ -1,27 +1,25 @@
-#include<iostream>
-#include<stack>
+
+#include <iostream>
+#include <stack>
+#include <vector>
 using namespace std;
 
-vector<int>nextgreaterelement(vector<int>arr){
-    stack<int>st;
-    int n = arr(size);
-    vector<int>nge(n);
+vector<int> nextgreaterelement(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> nge(n);
+    stack<int> st;
 
-    for(int i=0;i<n;i++){
-        while(!st.empty() && st.top()>=arr[i]){
+    for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= arr[i]) {
             st.pop();
         }
-        if(st.empty()){
-            nge[i]= -1;
-        }
-        else{
-            nge[i]=st.top();
+        if (st.empty()) {
+            nge[i] = -1;
+        } else {
+            nge[i] = st.top();
         }
         st.push(arr[i]);
     }
     return nge;
 }
 
-int main(){
-     
-}
